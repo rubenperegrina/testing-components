1efa555e902b11ebc61b453d9ac4a5e5050db32f
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-04-04 10:08:07 +0200

    Init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductsComponent } from './components/products/products.component';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { TokenInterceptor } from './interceptors/token.interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
