6af04eb4215648c03344694360a43ddccda7bb85
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-04-04 12:06:38 +0200

    Add tests for person input

--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Person } from 'src/app/models/person.model';
 
 @Component({
   selector: 'app-person',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./person.component.scss']
 })
 export class PersonComponent {
-
+  @Input() person!: Person;
 }
