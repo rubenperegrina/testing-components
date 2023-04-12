04a871d12aa9e071e695a1a534e0f1b6e2b1dbab
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-04-12 15:11:46 +0200

    Add tests for pipes

--- src/app/pipes/reverse.pipe.spec.ts
+++ src/app/pipes/reverse.pipe.spec.ts
@@ -1,6 +1,10 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { ReversePipe } from './reverse.pipe';
 
-describe('ReversePipe', () => {
+describe('ReservePipe', () => {
   it('create an instance', () => {
     const pipe = new ReversePipe();
     expect(pipe).toBeTruthy();
@@ -17,4 +21,58 @@ describe('ReversePipe', () => {
     const rta = pipe.transform("123");
     expect(rta).toEqual("321");
   })
+});
+
+@Component({
+  template: `
+    <h5>{{ 'amor' | reverse }}</h5>
+    <input [(ngModel)]="text">
+    <p>{{ text | reverse }}</p>
+  `
+})
+class HostComponent {
+  text = '';
+}
+
+describe('ReversePipe from HostComponent', () => {
+
+  let component: HostComponent;
+  let fixture: ComponentFixture<HostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HostComponent, ReversePipe],
+      imports: [FormsModule]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should the h5 be "roma"', () => {
+    const h5De = fixture.debugElement.query(By.css('h5'));
+    expect(h5De.nativeElement.textContent).toEqual('roma');
+  });
+
+  it('should apply reverse pipe when typing in the input', () => {
+    const inputDe = fixture.debugElement.query(By.css('input'));
+    const inputEl: HTMLInputElement = inputDe.nativeElement;
+    const pDe = fixture.debugElement.query(By.css('p'));
+
+    expect(pDe.nativeElement.textContent).toEqual('');
+
+    inputEl.value = 'ANA 2'; // 2 ANA
+    inputEl.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(pDe.nativeElement.textContent).toEqual('2 ANA');
+  });
 });
